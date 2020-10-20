5b5ba71bde0239b51d45fc67dac6cd97c9cdb380
Sel Embee <64166375+Selanda@users.noreply.github.com>
2020-10-20 13:34:36 -0400

    Update MSremover.bat
    Added missing quote marks to lines 11-13.

--- MSremover.bat
+++ MSremover.bat
@@ -8,9 +8,9 @@ echo Killing the running process....
 taskkill.exe /im msas2009.exe
 
 echo Removing the infected folders and files...
-rmdir /S /Q C:\Program Files\Antivirus 2009
-rmdir /S /Q C:\Documents and Settings\All Users\Application Data\CrucialSoft Ltd
-rmdir /S /Q C:\Documents and Settings\%username%\Start Menu\Programs\MS AntiSpyware 2009
+rmdir /S /Q "C:\Program Files\Antivirus 2009"
+rmdir /S /Q "C:\Documents and Settings\All Users\Application Data\CrucialSoft Ltd"
+rmdir /S /Q "C:\Documents and Settings\%username%\Start Menu\Programs\MS AntiSpyware 2009"
 
 echo The system should be clean now.
 pause
